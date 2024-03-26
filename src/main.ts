a8b4c6b97639739150bfdc831e07a7407dcd32c7
Quadri <kazeemquadri75@gmail.com>
2024-03-26 09:22:12 +0000

    email otp

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,11 @@ import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ErrorMiddleware } from './middleware/Error';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { v2 as cloudinary } from "cloudinary";
-require("dotenv").config();
-
+import { v2 as cloudinary } from 'cloudinary';
+import {config as dotenvConfig} from "dotenv"
+// require('dotenv').config();
+dotenvConfig()
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -35,13 +37,12 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/api/docs', app, document);
 
-
-// cloudinary config
-cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.CLOUD_API_KEY,
-  api_secret: process.env.CLOUD_SECRET_KEY,
-});
+  // cloudinary config
+  cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_API_KEY,
+    api_secret: process.env.CLOUD_SECRET_KEY,
+  });
 
   await app.listen(process.env.PORT);
 }
