5c62219c279067cdb0ac58e9ecf59e8780170bb5
Your Name <you@example.com>
2024-05-23 16:29:51 +0100

    updated

--- src/authentication/authentication.service.ts
+++ src/authentication/authentication.service.ts
@@ -202,7 +202,7 @@ export class AuthenticationService {
     user.resetPasswordExpires = new Date(Date.now() + 3600000); // 1 hour from now
     await user.save();
 
-    const resetUrl = `http://yourfrontend.com/reset-password?token=${resetToken}&email=${email}`;
+    const resetUrl = `${process.env.FRONTEND_URL}/reset-password?token=${resetToken}&email=${email}`;
     console.log('this is', resetToken);
     const data = { user: { name: user.name }, resetUrl };
 
