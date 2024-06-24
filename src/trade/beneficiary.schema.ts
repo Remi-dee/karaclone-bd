b48e9c1fdc40a998f8b847ac8d674a17183e6b95
Your Name <you@example.com>
2024-06-24 16:47:51 +0100

    updated

--- src/trade/beneficiary.schema.ts
+++ src/trade/beneficiary.schema.ts
@@ -14,6 +14,21 @@ export class Beneficiary extends Document {
 
   @Prop({ required: true })
   bank_name: string;
+
+  @Prop({ required: false })
+  currency: string;
+
+  @Prop({ required: false })
+  swift_code: string;
+
+  @Prop({ required: false })
+  ACH_Routing: string;
+
+  @Prop({ required: false })
+  Account_Type: string;
+
+  @Prop({ required: true })
+  bank_address: string;
 }
 
 export const BeneficiarySchema = SchemaFactory.createForClass(Beneficiary);
